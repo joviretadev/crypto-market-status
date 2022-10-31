4e4ac3c91f6e4162670a37c214228d2aa4e84810
rastatay <j.rendon@sesametime.com>
2022-10-31 18:43:40 +0100

    API connect

--- Components/Home.js
+++ Components/Home.js
@@ -1,10 +1,46 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
+import React, { useEffect, useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+const BGColor = "#000000"
+const styles = StyleSheet.create({
+    coinsText: {
+        color: '#ffff'
+    }
+   });
 
 export default function Home() {
 
+    const [coins, setCoins] = useState([])
+
+    const loadData = async() => {
+        const response = await fetch("https://api.coingecko.com/api/v3/coins/list");
+        const data = await response.json()
+        setCoins(data)
+        //console.log(data);
+    }
+    useEffect(() =>{
+       loadData();
+    }, [])
     return (
-        <View>
-            <Text>Hola desde Home</Text>
+        
+        <View style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            backgroundColor: BGColor,
+        }}>
+            <StatusBar style='light'/>
+            
+            <FlatList
+            data = {coins}
+            renderItem={({item}) =>{
+                return <Text style={styles.coinsText}>{item.name}</Text>
+            }}
+            />
+           
         </View>
     );
 }
